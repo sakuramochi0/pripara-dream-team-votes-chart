0ab4e4bc29f901ef23bf08463e0a065ce5fc2801
sakuramochi <sakuramochi.mochi.0@gmail.com>
2018-01-03 10:47:12 +0900

    各順位と圏外のラベルを適切に表示。Githubリポジトリへのリンクの追加。

--- js/main.js
+++ js/main.js
@@ -12,12 +12,22 @@ function handleQueryResponse(response) {
     const data = response.getDataTable();
     // const chart = new google.charts.Line(document.getElementById('chart'));
     const chart = new google.visualization.LineChart(document.getElementById('chart'));
+    var ticks = [];
+    for (var i = 1; i <= 15; i++) ticks.push(i);
+    ticks.push(
+        { v: 0, f: '' }, // 1位の上の余白
+        { v: 20, f: '圏外' },
+        { v: 21, f: '' } // 一番下の余白
+    );
     const options = {
         theme: 'material',
         width: '100%',
         height: 800,
-        chartArea: { width: '60%', height: '80%', right: '28%' },
-        vAxis: { direction:-1, minValue: 1, maxValue: 16 },
+        chartArea: { width: '60%', height: '80%', right: '29%' },
+        vAxis: {
+            direction:-1,
+            ticks: ticks,
+        },
         pointSize: 8,
         lineWith: 4,
     };
