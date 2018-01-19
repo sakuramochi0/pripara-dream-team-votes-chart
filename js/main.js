5ba96a8b7576e8a64187f162a9f599b8187acdf8
sakuramochi <sakuramochi.mochi.0@gmail.com>
2018-01-19 20:05:16 +0900

    キャラの順序を1週目の順位の順番に修正

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const URL = 'https://docs.google.com/spreadsheets/d/1WOwQf4tjLCslhMTnLFb44ATlDwIBBTlhCfscHlPRTfw/gviz/tq?gid=0&headers=1&range=A:S';
 
-google.charts.load('current', { packages:['corechart'], language: 'ja' });
+google.charts.load('current', {packages: ['corechart'], language: 'ja'});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
@@ -15,28 +15,29 @@ function handleQueryResponse(response) {
     var ticks = [];
     for (var i = 1; i <= 15; i++) ticks.push(i);
     ticks.push(
-        { v: 0, f: '' }, // 1位の上の余白
-        { v: 20, f: '圏外' },
-        { v: 21, f: '' } // 一番下の余白
+        {v: 0, f: ''}, // 1位の上の余白
+        {v: 20, f: '圏外'},
+        {v: 21, f: ''} // 一番下の余白
     );
     // 次のキャラ順に色を割り当てること
-    // あろま らぁら みかん みれぃ ドロシー
-    // ファララ そふぃ コヨイ ひびき ゆい
-    // シオン のん ファルル ちり ショウゴ
-    // ガァララ みちる にの
+    // あろま らぁら みれぃ ひびき コヨイ
+    // みかん ファララ ゆい ドロシー そふぃ
+    // ファルル のん ガァララ みちる にの
+    // シオン ちり ショウゴ
     const lineColors = [
-        '#9C27B0','#EA80FC','#FF5252','#FFEB3B','#40C4FF',
-        '#64FFDA','#F50057','#1A237E','#673AB7','#FFF59D',
-        '#9C27B0','#FF80AB','#B2FF59','#009688','#FFF59D',
-        '#283593','#9E9E9E','#69F0AE',
+        '#9C27B0', '#EA80FC', '#FFEB3B', '#673AB7', '#1A237E',
+        '#FF5252', '#64FFDA', '#FFF59D', '#40C4FF', '#F50057',
+        '#B2FF59', '#FF80AB', '#283593', '#9E9E9E', '#69F0AE',
+        '#9C27B0', '#009688', '#FFF59D',
+
     ];
     const options = {
         theme: 'material',
         width: '100%',
         height: 800,
-        chartArea: { width: '60%', height: '80%', right: '29%', top: '5%' },
+        chartArea: {width: '60%', height: '80%', right: '29%', top: '5%'},
         vAxis: {
-            direction:-1,
+            direction: -1,
             ticks: ticks,
         },
         pointSize: 8,
